c1f737ae24046f4b4cf4fda1341e2cecc78e76ba
Jane Yang <j7yang@ucsd.edu>
2023-09-08 13:55:40 -0500

    updated vis_streams_multiwork for equal interval vis panels

--- data-vis/vis_streams_multiwork.m
+++ data-vis/vis_streams_multiwork.m
@@ -1,4 +1,4 @@
-function vis_streams_multiwork(subexpIDs, vars, streamlabels, directory, args)
+function vis_streams_multiwork_v2(subexpIDs, vars, streamlabels, directory, args)
 % see demo_vis_streams_multiwork for documentation
 if ischar(subexpIDs) && contains(subexpIDs, 'demo')
     switch subexpIDs
@@ -39,11 +39,30 @@ for s = 1:numel(subs)
                 window_times_file = fullfile(subpaths{s}, [args.window_times_variable '.mat']);
             else
                 window_times_file = args.window_times_variable;
+
             end
         elseif ismember(subtable(s,2), [12])
             window_times_file = [];
         else
             window_times_file = fullfile(subpaths{s}, 'cevent_trials.mat');
+
+            % get the time mitrix from this file 
+            data = load_data_from_file(window_times_file);
+           
+            % Quadrature the entire timeline and store it as a 1 x 1 struct
+            first_onset = data(1,1);
+            last_offset = data(end, 2);
+            total_length = last_offset - first_onset;
+            avg_length = ceil(total_length / 4);
+            sdata = struct('data',[first_onset, first_onset + avg_length, 1;
+                    first_onset + avg_length, first_onset + 2*avg_length, 2;
+                    first_onset + 2*avg_length, first_onset + 3*avg_length, 3;
+                    first_onset + 3*avg_length, first_onset + 4*avg_length, 4;]);
+
+            % save the quartered mitrix as a temporarily file
+            save("new_cevent_trials",'sdata');
+
+            window_times_file = 'new_cevent_trials.mat';
         end
         flag_dir = 1;
         if exist('directory', 'var') && ~isempty(directory)
@@ -59,6 +78,9 @@ for s = 1:numel(subs)
         
         args.titlelabel = sprintf('%d', subs(s));
         h = vis_streams_files(filenames, window_times_file, savefilename, streamlabels, args);
+
+        % delete the temporarily file
+        delete(window_times_file)
         if flag_dir
             close(h);
         end
