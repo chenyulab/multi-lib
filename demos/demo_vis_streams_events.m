1e216c1ecde9504a5ca40a6f9bec6165b661192a
Jingwen Pang <pangjingwen1999@163.com>
2024-09-11 15:30:55 -0500

    Update demo_vis_streams_events.m

--- demos/demo_vis_streams_events.m
+++ demos/demo_vis_streams_events.m
@@ -1,6 +1,6 @@
 %%%
 % Author: Jane Yang
-% Last Modified: 06/04/2024
+% Last Modified: 09/11/2024 by Brianna Kaplan
 % This is the demo function for vis_streams_events() function.
 %
 % 
@@ -99,6 +99,23 @@ function demo_vis_streams_events(option)
             % This case shows parent's speech, parent's gaze, child's inhand, and parent's inhand that
             % happened within 3 seconds after each child's gaze instance
             % only instances on a subset of objects are displayed
+           vis_streams_events(subexpID,cevent_name,var_list,cevent_values,streamlabels,output_file_dir,args);
+    case 7
+           % This case shows what happened 3 seconds before and after a child face look
+
+           subexpID = 15;
+           cevent_name = 'cevent_eye_roi_child'; % the variable you want to constrain by time or object
+           cevent_values = [11]; % the objects you want to plot for the cevent, here we only want to plot face looks
+
+           % Other variables you want to plot. Won't be constrained by cevent_values- this is why we plot child roi twice, to see what else the child is looking at during the 6 second window 
+           % If youdo want to constrain these by an object use
+           % args.showSingleColor or args.displayObjList
+           var_list = {'cevent_eye_roi_child', 'cevent_eye_roi_parent', 'cevent_eye_joint-attend_both', 'cevent_inhand_child','cevent_inhand_parent','cevent_speech_utterance'};
+           streamlabels = {'ceye','ceye','peye','ja','chand','phand','speech'}; % variable labels for sevent and other vars
+           output_file_dir = 'Z:\CORE\scheduled_tasks\multi-lib\demo_results\vis_streams_events\case7';
+
+           args.whence = 'start';
+           args.interval = [-3 3]; % 3 seconds before and after the face look
            vis_streams_events(subexpID,cevent_name,var_list,cevent_values,streamlabels,output_file_dir,args);
     end
 end
