b71a57b414aa508217a7cefbd8d15ff026c7b73d
Jingwen Pang <pangjingwen1999@163.com>
2024-10-22 15:35:29 -0500

    Update extract_basic_demographic.m
    fixed the bug that only read the first wave age

--- data-access/extract_basic_demographic.m
+++ data-access/extract_basic_demographic.m
@@ -1,5 +1,6 @@
-% Author: Ruchi Shah
-% Last modified: 03/20/2024
+% Original Author: Ruchi Shah
+% Modifier: Jingwen Pang
+% Last modified: 10/22/2024
 % Summary
     % This function creates a .tsv containing demographic information
     % derived from the subject_table and the inputted demographic_survey
@@ -17,57 +18,74 @@
     %       BirthMonth
    
 function extract_basic_demographic()
-    data = readtable(fullfile(get_multidir_root(),'HOME_survey.xlsx'));
-    subTable = read_subject_table(); 
-    kid_ids = data{:, 2};
-    % find the subjects in the subject table that demographic_survey
-    % kid_ids correspond to
-    idx = ismember(subTable(:, 4), kid_ids); 
-    filtered_subjTable = subTable(idx, :);
-    % get the experiments subjects are a part of
-    experiments = unique(filtered_subjTable(:, 2));
+    exp_ids = [351 353 361 362 363];
+    exp_type = 'HOME2';
+    survey_dir = 'HOME_survey.xlsx';
+    tsv_headers = {'subject','kid_id','gender','age_at_experiment','birth_year','birth_month'};
+    tsv_type = {'int32', 'int32', 'string', 'double','int32','int32'};
 
-    for i = 1:length(experiments)
-        % currently only for experiment 351 and 353
-        if experiments(i) ~= 351 && experiments(i) ~= 353
-            continue;
-        end
-            exp_idx = filtered_subjTable(:, 2) == experiments(i);
-            exp_filtered_table = filtered_subjTable(exp_idx, :);
-            sub_id_col = exp_filtered_table(:, 1); 
-            kid_id_col = exp_filtered_table(:, 4); 
-            [match, loc] = ismember(kid_id_col, data{:, 2});
-            filtered_survey = data(loc(match), :);
-    
-            % gender column
-            gender_col = filtered_survey{:, 7};
-            gender_col = cellfun(@(x) x(1), gender_col, 'UniformOutput', false);
-        
-            % Calculate age at experiment, extract birth year and month
-            date_of_experiment = filtered_survey{:, 1};
-            dob = filtered_survey{:, 4}; 
+    data = readtable(fullfile(get_multidir_root(),survey_dir));
 
-            age_in_months = calmonths(between(dob, date_of_experiment, 'months'));
-            remaining_days = caldays(between(dob + calmonths(age_in_months), date_of_experiment));
-            fractional_month = remaining_days / 31;
-            age_at_experiment = age_in_months + fractional_month;
-            age_at_experiment = round(age_at_experiment, 1);
+    for e = 1:length(exp_ids)
+        exp_id = exp_ids(e);
+        sub_ids = cIDs(exp_id);
 
-            birth_year = year(dob);
-            birth_month = month(dob);
-    
-            % Create results table
-            resultsTable = table(sub_id_col(match), kid_id_col(match), gender_col, age_at_experiment, ...
-                                 birth_year, birth_month, ...
-                                 'VariableNames', {'subject','kid_id','gender','age_at_experiment','birth_year','birth_month'});
+        tsv_table = table('Size', [0 length(tsv_headers)],'VariableTypes', tsv_type, 'VariableNames', tsv_headers);
+
+        for s = 1:length(sub_ids)
+            sub_id = sub_ids(s);
+
+            % map with the data in Home survey corresponding row
+            sub_info = get_subject_info(sub_id);
+            kid_id = sub_info(:,4);
+            date = sub_info(:,3);
+
+            % Convert the number to string and extract year, month, and day
+            yearStr = num2str(floor(date / 10000));  % First 4 digits for the year
+            monthStr = num2str(mod(floor(date / 100), 100));  % Middle 2 digits for the month
+            dayStr = num2str(mod(date, 100));  % Last 2 digits for the day
+            dateObj = datetime([yearStr '-' monthStr '-' dayStr], 'InputFormat', 'yyyy-MM-dd');
+            date = datestr(dateObj, 'dd-mmm-yyyy');
+
+            idx = data{:,2} == kid_id & data{:,1} == date & strcmp(data{:,3}, exp_type);
+            if any(idx)
+                filtered_survey = data(idx, :);
+                % disp(filtered_survey);
+                % gender column
+                gender = filtered_survey{:, 7};
+                gender = gender{1};
+                gender = gender(1);
             
-            % Directory setup and output (.tsv)
-            exp_dir = get_experiment_dir(experiments(i));
-            directory = fullfile(exp_dir, 'survey_data');
-            if ~exist(directory, 'dir')
-               mkdir(directory);
+                % Calculate age at experiment, extract birth year and month
+                date_of_experiment = filtered_survey{:, 1};
+                dob = filtered_survey{:, 4}; 
+    
+                age_in_months = calmonths(between(dob, date_of_experiment, 'months'));
+                remaining_days = caldays(between(dob + calmonths(age_in_months), date_of_experiment));
+                fractional_month = remaining_days / 31;
+                age_at_experiment = age_in_months + fractional_month;
+                age_at_experiment = round(age_at_experiment, 1);
+    
+                birth_year = year(dob);
+                birth_month = month(dob);
+        
+                % Create results table
+                subject_row = {sub_id, kid_id, gender, age_at_experiment, ...
+                                     birth_year, birth_month};
+    
+                tsv_table(end+1, :) = subject_row;
+            else
+                fprintf('survey not found for %d\n', sub_id)
             end
-            filePath = fullfile(directory, 'basic.tsv');
-            writetable(resultsTable,filePath, 'filetype','text', 'delimiter','\t');
+        end
+            
+        % Directory setup and output (.tsv)
+        exp_dir = get_experiment_dir(exp_id);
+        directory = fullfile(exp_dir, 'survey_data');
+        if ~exist(directory, 'dir')
+           mkdir(directory);
+        end
+        filePath = fullfile(directory, 'basic.tsv');
+        writetable(tsv_table,filePath, 'filetype','text', 'delimiter','\t');
     end
 end
