22a83a49437db10f24acb168087a39b4253f3384
Jingwen Pang <pangjingwen1999@163.com>
2024-11-11 14:25:39 -0600

    update start up functions

--- data-utility/scheduled_tasks/run_m2html.m
+++ data-utility/scheduled_tasks/run_m2html.m
@@ -1,2 +1,2 @@
-cd('Z:/CORE/scheduled_tasks/multi-lib');
+cd('Z:/CORE/repository_new/multi-lib');
 m2html('recursive', 'on', 'tabs', 4, 'globalHypertextLinks', 'on', 'htmlDir', 'Z:/CORE/html');
--- global_matlab_startup.m
+++ global_matlab_startup.m
@@ -1,6 +1,7 @@
 % every computer will execute a startup.m in the initial startup folder. This file does runs the following,
 %
-% addpath(genpath('r:\repository\multi-lib\'));
+% addpath(genpath('r:\repository_new\multi-lib\'));
 % matlab_startup
 % 
-% therefore, if you want all computers to run additional startup commands, place them here. 
+% therefore, if you want all computers to run additional startup commands, place them here.
+disp('all the functions are added to path, you are all set!')
