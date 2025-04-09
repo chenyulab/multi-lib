8e8dd4bdc4bb7448c4e35bb34a94fc099642ddfe
Jingwen Pang <pangjingwen1999@163.com>
2025-04-09 10:11:48 -0500

    update speech in situ function

--- data-analysis/modules/speech/speech_cevent/group_speech_in_situ.m
+++ data-analysis/modules/speech/speech_cevent/group_speech_in_situ.m
@@ -1,140 +1,125 @@
 %%%
-% Author: Jane Yang
-% Last Modifier: 3/27/2023
-% This function calls extract_speech_in_situ and group data into subject
-% or experiment level, outputting target word count and showing distribution
-% of words.
+% Author: Jingwen Pang
+% last modified: 3/20/2025
 % 
-% Input: expID, cevent variable name, category (object ID), 
-%        a list of target words, output filename, grouping option, and
-%        option 'whence' and 'interval' arguments for shifting timestamps.
-% Output: a table containing expID, subID, category (objID), 
-%         target words frequency, aggregated utterances. A .CSV file is 
-%         generated based on the output table.
+% This function take the speech in situ csv file and group them based in
+% subject-object level, subject level, and object level
+%
+%
+%
+% example call:
+%   input_csv_name = 'exp351_cevent-roi_speech-babyname.csv';
+%   group_speech_in_situ(input_csv_dir)
+% 
 %%%
+function [table_sub, table_cat, table_sub_cat] = group_speech_in_situ(input_csv,args)
+    % remove extension
+    file_label = input_csv(1:end-4);
 
-
-function grouped_instance = group_speech_in_situ(expID,cevent_var,category,target_words,output_filename,option,args)
-    %% parameter checking
-    % check if 'whence' and 'interval'
     if ~exist('args', 'var') || isempty(args)
-        args = struct([]);
+        args = struct();
     end
 
-    % threshold for filtering cevent instances that are less than N seconds long
-    if isfield(args, 'threshold')
-        threshold = args.threshold;
-    else
-        threshold = 0;
-    end
-
-    if isfield(args, 'whence')
-        whence = args.whence;
-    else
-        whence = '';
-    end
-
-    if isfield(args, 'interval')
-        interval = args.interval;
-    else
-        interval = [0 0];
-    end
-
-    % generate column names
-    colNames = {'expID','subID','category'};
-    for i = 1:numel(target_words)
-        colNames{end+1} = char(target_words(i));
-    end
-    colNames{end+1} = 'utterances';
-
-    colNames_exp = {'expID','category'};
-    for i = 1:numel(target_words)
-        colNames_exp{end+1} = char(target_words(i));
-    end
-    colNames_exp{end+1} = 'utterances';
-
-
-    grouped_instance = [];
-    % group instances at level specified by 'option' argument
-    if strcmp(option,'subject')
-        overall_instance = extract_speech_in_situ(expID,cevent_var,category,target_words,'',args);
-
-        % list unique subjects in the return table
-        sub_list = str2double(unique(overall_instance(:,2)));
-
-        for i = 1:numel(sub_list)
-            % get all instances for current subject
-            sub_instance = overall_instance(strcmp(overall_instance(:,2),string(sub_list(i))),:);
-
-            % group utterances together per subject
-            combined_utt = strtrim(strjoin(sub_instance(:,end)));
-
-            % count target word frequency
-            count_vec = get_target_words_count(target_words,strjoin(sub_instance(:,end)));
-
-            % generate an instance entry per subject
-            instance = [expID sub_list(i) category count_vec combined_utt];
-
-            % append new instance to a list of instances
-            grouped_instance = [grouped_instance;instance];
-
-            %%% TODO: get combined utterances word count and show
-            %%% distribution
-
-            % % get word count for all words in the combined utterance
-            % combined_utt_count_table = wordCloudCounts(combined_utt); 
-            % 
-            % % generate word cloud
-            % wordcloud(combined_utt);
-            % title(num2str(sub_list(i)));
-            % saveas(gcf,['wordcloud_' num2str(sub_list(i)) '_obj' num2str(category) '.png']);
-        end
-    elseif strcmp(option,'experiment')
-        overall_instance = extract_speech_in_situ(expID,cevent_var,category,target_words,'',args);
-
-        % trival step, get unique experiment ID just in case
-        exp_list = str2double(unique(overall_instance(:,1)));
-        
-        for i = 1:numel(exp_list)
-            % get all instances for current subject
-            sub_instance = overall_instance(strcmp(overall_instance(:,1),string(exp_list(i))),:);
-
-            % group utterances together per subject
-            combined_utt = strtrim(strjoin(sub_instance(:,end)));
-
-            % count target word frequency
-            count_vec = get_target_words_count(target_words,strjoin(sub_instance(:,end)));
-
-            % generate an instance entry per subject
-            instance = [exp_list(i) category count_vec combined_utt];
-            % append new instance to a list of instances
-            grouped_instance = [grouped_instance;instance];
-
-            %%% TODO: get combined utterances word count and show
-            %%% distribution
-
-            % % get word count for all words in the combined utterance
-            % combined_utt_count_table = wordCloudCounts(combined_utt);
-            % 
-            % % generate word cloud
-            % wordcloud(combined_utt);
-            % title("exp"+num2str(exp_list(i)));
-            % saveas(gcf,['wordcloud_' 'exp_' num2str(exp_list(i)) '_' num2str(category) '.png']);
-        end
-    else
-        fprintf('Please specify a level of grouping: choose to group data in subject-level or experiment-level\n.');
-    end
-
-    % write output CSV file
-    if strcmp(option,'subject')
-        grouped_instance = array2table(grouped_instance,'VariableNames',colNames);
-    elseif strcmp(option,'experiment')
-        grouped_instance = array2table(grouped_instance,'VariableNames',colNames_exp);
+    if ~isfield(args, 'exp_col')
+        args.exp_col = 1;
     end
     
-    % don't write to a CSV file if the function is used as an intermediate
-    % helper function
-    if ~strcmp(output_filename,'')
-        writetable(grouped_instance,output_filename);
+    if ~isfield(args, 'sub_col')
+        args.sub_col = 2;
+    end
+    
+    if ~isfield(args, 'cat_col')
+        args.cat_col = 6;
+    end
+    
+    if ~isfield(args, 'trial_time_col')
+        args.trial_time_col = 3;
+    end
+    exp_col = args.exp_col;
+    sub_col = args.sub_col;
+    cat_col = args.cat_col;
+    trial_time_col = args.trial_time_col;
+    
+    data = readtable(input_csv);
+    
+    exp_ids = unique(table2array(data(:,exp_col)));
+    sub_ids = unique(table2array(data(:,sub_col)));
+    cat_values = unique(table2array(data(:,cat_col)));
+    
+    for e = 1:length(exp_ids)
+        exp_id = exp_ids(e);
+        exp_data = data(data{:,exp_col}==exp_id,:);
+    
+        sub_cat = {};
+        sub = {};
+        cat = {};
+    
+        % subject level
+        for s = 1:length(sub_ids)
+            sub_id = sub_ids(s);
+            sub_data = exp_data(exp_data{:,sub_col}==sub_id,:);
+            trial_time = sub_data{1,trial_time_col};
+
+            % subject-category level
+            for c = 1:length(cat_values)
+                cat_value = cat_values(c);
+                cat_data = sub_data(sub_data{:,cat_col}==cat_value,:);
+                
+                if ~isempty(cat_data)
+                    num_instance = size(cat_data,1);
+                    
+                    % remove empty text
+                    clean_utt = table2cell(cat_data(:,end));
+                    clean_utt = clean_utt(~cellfun('isempty', clean_utt));
+        
+                    utterances = strjoin(clean_utt,' ');
+                    sub_cat = [sub_cat;{exp_id,sub_id,cat_value,trial_time,num_instance,utterances}];
+                end
+            end
+    
+            num_instance = size(sub_data,1);
+    
+            % remove empty text
+            clean_utt = table2cell(sub_data(:,end));
+            clean_utt = clean_utt(~cellfun('isempty', clean_utt));
+    
+            utterances = strjoin(clean_utt,' ');
+            sub = [sub;{exp_id,sub_id,trial_time,num_instance,utterances}];
+    
+        end
+    
+        % category level
+        for c = 1:length(cat_values)
+            cat_value = cat_values(c);
+            cat_data = exp_data(exp_data{:,cat_col}==cat_value,:);
+    
+            num_instance = size(cat_data,1);
+                
+            % remove empty text
+            clean_utt = table2cell(cat_data(:,end));
+            clean_utt = clean_utt(~cellfun('isempty', clean_utt));
+    
+            utterances = strjoin(clean_utt,' ');
+            cat = [cat;{exp_id,cat_value,num_instance,utterances}];
+    
+        end
+    
+        % write output file into table
+        sub_cat_name = sprintf("%s_subject-category.csv",file_label);
+        col_names = {'expID','subID', 'catValue', 'trial_time', 'instance#', 'text'};
+        table_sub_cat = cell2table(sub_cat, 'VariableNames', col_names);
+        writetable(table_sub_cat, sub_cat_name);
+    
+        sub_name = sprintf("%s_subject.csv",file_label);
+        col_names = {'expID','subID', 'trial_time', 'instance#', 'text'};
+        table_sub = cell2table(sub, 'VariableNames', col_names);
+        writetable(table_sub, sub_name);
+    
+        cat_name = sprintf("%s_category.csv",file_label);
+        col_names = {'expID','catValue', 'instance#', 'text'};
+        table_cat = cell2table(cat, 'VariableNames', col_names);
+        writetable(table_cat, cat_name);
+    
+    
     end
 end
