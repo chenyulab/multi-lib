93a8066ef8649d7d28ec3a7055c9856edc78bbb0
Jingwen Pang <pangjingwen1999@163.com>
2025-06-14 10:35:50 -0500

    Update get_num_obj.m

--- data-access/get_num_obj.m
+++ data-access/get_num_obj.m
@@ -23,10 +23,15 @@ rtr_list = [];
 for e = 1:length(exp_id)
     exp = exp_id(e);
     row = stim_data(stim_data.experiment_id == exp,:);
-    if isempty(varargin)
-        total_n = row.number_of_objs_local;
+    if isempty(row)
+        warning(sprintf('exp_%d is not found in stimuli table', exp))
+        total_n = 0;
     else
-        total_n = row.number_of_objs_global;
+        if isempty(varargin)
+            total_n = row.number_of_objs_local;
+        else
+            total_n = row.number_of_objs_global;
+        end
     end
     rtr_list = [rtr_list, total_n];
 end
