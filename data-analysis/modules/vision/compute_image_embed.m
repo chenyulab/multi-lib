f2a164cd33b854e83cffeeb6bf83c5a4e9df8b5b
eltongmt <eltongmt@gmail.com>
2025-08-22 13:35:14 -0500

    Updated image sim functions for training and embedding

--- data-analysis/modules/vision/compute_image_embed.m
+++ data-analysis/modules/vision/compute_image_embed.m
@@ -1,46 +1,40 @@
 %% 
-% Author: Elton Martinez
-% Modifier: Eric Zhao
-% Last modified: 8/8/2025
-%
 % Takes in a camera directory and outputs a table of all image frames 
 % located in category subfolders under that directory
 %
-% Input parameters:
+% Author: Elton Martinez
+% Modifier: Elton Martinez
+% Last modified: 8/22/2025
+%
+% Parameters:
 % - subexpID
 %   integer array of subjects or an exp number
-% - net
-%   the initialized resnet or the resnet path
-% - in_dir
-%   path of directory, where directory contains subfolders(cam directory)
-%   files within subfolders should be formatted with frame number after an underscore and before the file extension 
-%   e.g. for frame 12, 'image_12.jpg' or 'anything_1234_12.ext'    
-%
 % Output: 
 %   outputs table with variables: 
-%   frame_name(string), name of image file
-%   category_folder_name(string), name of folder containing image file
-%   obj_id(double), object/category number
-%   frame_id(double), frame number
-%   image_embedding(gpuArray single), vector representation of image
-%   image_identifier(double), object number and frame number represented as one number
+%       frame_name(string), name of image file
+%       category_folder_name(string), name of folder containing image file
+%       obj_id(double), object/category number
+%       frame_id(double), frame number
+%       image_embedding(gpuArray single), vector representation of image
+%       image_identifier(double), object number and frame number represented as one number
 %%
 
-%maybe have an input parameter to specify if want for cam directory or just category directory
-function compute_image_embed_cID(subexpID,net)
-    sub_list = cIDs(subexpID);
-    
-    %if inputted path to net instead of initialized net
-    if ischar(net) || isstring(net) || iscellstr(net)
-        net = get_resnet(net);
+function compute_image_embed(subexpID)
+    subs = cIDs(subexpID);
+    expID = sub2exp(subs(1));
+
+    net = get_resnet(expID);
+
+    if isempty(net)
+        return 
     end
 
-    for sub = sub_list'
+    % compute embedding list 
+    for sub = subs'
         subject_directory = get_subject_dir(sub);
         cam_directory = fullfile(subject_directory,'cam07_attended-objs-frames_p');
         
         output_directory = fullfile(subject_directory,'extra_p');
-        % output_directory = "Z:\EricZ\Obj Similarity\data\subject_dataframes"; %testline
         output_filename = sprintf('%d_child_attended-objs-frames_image_scores.mat',sub);
         output_filename = fullfile(output_directory,output_filename);
         
@@ -48,20 +42,22 @@ function compute_image_embed_cID(subexpID,net)
             fprintf('skipped subject %d, already has file\n',sub)
             continue
         end
-
-        dir_df = compute_image_embed(cam_directory,net);
         
+        % actually find the embedding 
+        fprintf('\nProcessing: %d\n', sub);
+        tic
+        dir_df = compute_image_embed_helper(cam_directory,net);
+        toc
+        
+        % save as .mat file in the extra_p
         save(output_filename, 'dir_df');
+        fprintf('Saved: %s\n', output_filename)
 
     end
 end
 
-function image_embed_df = compute_image_embed(in_dir,net) 
+function image_embed_df = compute_image_embed_helper(in_dir,net) 
     parallel.gpu.enableCUDAForwardCompatibility(true);
-    
-    % in_dir = 'M:\experiment_351\included\__20240921_10118\cam07_attended-objs-frames_p'; %testline
-    % in_dir = 'M:\experiment_351\included\__20230214_10055\cam07_attended-objs-frames_p'; %testline
-    % in_dir = 'M:\experiment_91\included\__20161130_9101\cam07_attended-objs-frames_p'; %testline
     
     image_embed_df = table(); % initialize eventual output
 
@@ -87,24 +83,7 @@ function image_embed_df = compute_image_embed(in_dir,net)
 
         directory = directory'; % turn into vert array, because code below doesnt work with horiz
         
-        %% OLD: requires files formatted as 'obj_{obj_id}_{frame_id}.jpg' e.g. 'obj_1_1.jpg'
-        % dir_split = cellfun(@(x) split(x, "_"), directory, 'UniformOutput', false);
-        % % dir_df = dir_split; %debugline
-        % dir_df = cell2table(horzcat(dir_split{:})');
-        % num_ext = cellfun(@(x) split(x, "."), dir_df{:,3}, 'UniformOutput', false);
-        % num_ext = horzcat(num_ext{:})';
-        % dir_df.("frame_id") = num_ext(:,1);
-        % dir_df.("frame_name") = directory;
-        % dir_df = removevars(dir_df,{'Var1','Var3'});
-        % dir_df = renamevars(dir_df,"Var2","obj_id");
-        % 
-        % % dir_df.("frame_name") = fullfile(dir_df{:,"frame_id"},dir_df{:,"frame_name"});
-        % dir_df.('obj_id') = arrayfun(@(x) str2double(x{1}), dir_df{:,'obj_id'});
-        % dir_df.('frame_id') = arrayfun(@(x) str2double(x{1}), dir_df{:,'frame_id'});
-        % dir_df = sortrows(dir_df, {'obj_id','frame_id'},{'ascend'});
-        % full_dir_df = [full_dir_df;dir_df];
-        
-        %% NEW: takes in cam_directory and outputs table with: frame names, category folder names, obj id, frame id, img emb        
+        % takes in cam_directory and outputs table with: frame names, category folder names, obj id, frame id, img emb        
         %frame_name
         dir_df = cell2table(directory, 'VariableNames',{'frame_name'});    
         
@@ -132,21 +111,17 @@ function image_embed_df = compute_image_embed(in_dir,net)
         dir_df.('image_embedding') = cellfun(@(x) (get_image_embed(x,net)), dir_df.Row, 'UniformOutput', false);
         dir_df.image_embedding = cellfun(@(x) (gather(x)), dir_df.image_embedding, 'UniformOutput', false); %turn gpuarray into cpu because can't directly write gpuarray objects
 
-        % %image_identifier
-        % decimal_df = floor(log10(dir_df.frame_id)) + 1;
-        % decimal_df = dir_df.frame_id ./ 10 .^ decimal_df;
-        % dir_df.('image_identifier') = dir_df.obj_id + decimal_df;
 
         %concatenate table for each object
         image_embed_df = [image_embed_df;dir_df]; %#ok<AGROW>
     end
-    % full_dir_df = sortrows(full_dir_df, {'obj_id','frame_id'},'ascend'); %sort by objects then frame num
     image_embed_df = sortrows(image_embed_df, {'frame_id'},'ascend'); %sort only by frame num
 end
 
-function embedding = get_image_embed(I, net)
+% given an image path and resnet find its embedding 
+function embedding = get_image_embed(image_path, net)
     inputSize = net.Layers(1).InputSize;
-    I = imread(I);
+    I = imread(image_path);
     I = single(imresize(I,inputSize(1:2)));
     I = gpuArray(I);
     embedding = predict(net, I);
--- data-analysis/modules/vision/get_resnet.m
+++ data-analysis/modules/vision/get_resnet.m
@@ -1,10 +1,24 @@
-% removes last layer of net and returns initialized net
-function net = get_resnet(net_path)
-    [~, netname, ext] = fileparts(net_path);
-    fprintf("loading resnet:%s%s\n",netname, ext) %display net name
-    x = load(net_path);
-    net = x.net;
-    net = removeLayers(net, "fc1000_softmax");
-    net = initialize(net);
-    disp("finished")
+%% 
+% Returns a initialized attend-objs resnet without the last layer for the 
+% target experiment. If .mat file is not found returns an empty array
+%
+% Author: Elton Martinez
+% Modifier: Elton Martinez
+% Last modified: 8/22/2025
+%%
+
+function net = get_resnet(expID)
+    resnet_path = fullfile(get_multidir_root(), sprintf('experiment_%d',expID),'resnet_attend_objs.mat');
+
+    if isfile(resnet_path)
+        fprintf("loading resnet: %s\n", resnet_path) %display net name
+        x = load(resnet_path);
+        net = x.net;
+        net = removeLayers(net, "fc1000_softmax");
+        net = initialize(net);
+        fprintf("  finished\n")
+    else
+        fprintf('No resnet model found for exp %d\n', expID)
+        net = [];
+    end
 end
