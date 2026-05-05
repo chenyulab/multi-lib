ff17aee53a5a648225c04a067aac63c5e0f98527
Jingwen Pang <79369216+1925115102@users.noreply.github.com>
2026-05-05 13:59:37 -0500

    Update demo_speech_analysis_functions.m

--- demos/demo_speech_analysis_functions.m
+++ demos/demo_speech_analysis_functions.m
@@ -36,7 +36,7 @@
 %%%
 function demo_speech_analysis_functions(option)
     % all the demo files are saved into, users can define their own path:
-    output_dir = '..\demo_results\speech_analysis';
+    output_dir = 'Z:\CORE\repository_new\multi-lib\demo_results\speech_analysis';
     
     switch option
         %%% extracting functions
