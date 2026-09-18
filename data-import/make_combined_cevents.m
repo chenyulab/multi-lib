15a79e602e56db97b5b018a0bf36e87918b5cc97
chenyulab <chenyulab@gmail.com>
2026-09-18 15:05:17 -0500

    Update make_combined_cevents.m
    Updated function, so duplicate events with different categories do not get deleted

--- data-import/make_combined_cevents.m
+++ data-import/make_combined_cevents.m
@@ -56,29 +56,39 @@ function make_combined_cevents(subexpID, cevent1, cevent2, cevent_naming_matrix)
         exp_id = sub2exp(subs(s));
         if all(cellfun(@(a) has_variable(subs(s), a), {cevent1, cevent2}))
             gt = get_variable(subs(s), 'cstream_trials');
-            var1 = cevent2cstream(get_variable(subs(s),cevent1),30.0,1/30,0);
-            var2 = cevent2cstream(get_variable(subs(s),cevent2),30.0,1/30,0);
+            event1 = get_variable(subs(s),cevent1);
+            event2 = get_variable(subs(s),cevent2);
+            var1 = cevent2cstream(event1,30.0,1/30,0);
+            var2 = cevent2cstream(event2,30.0,1/30,0);
             [gt,var1,var2] = align_cstreams(gt,var1,var2);
+
+            % Store every category separately so categories with the same
+            % onset and offset are not removed by cevent2cstream.
+            categories = unique([event1(:,3); event2(:,3)]);
+            categories(categories == 0) = [];
+            var1_categories = make_category_cstreams(event1,var1(:,1),categories);
+            var2_categories = make_category_cstreams(event2,var2(:,1),categories);
+            var1_active = any(var1_categories,2);
+            var2_active = any(var2_categories,2);
 
             %% var1 and var2 same variables
             if ~(cevent_naming_matrix{1} == "")
-                both = [var1(:,2) var2(:,2)];
-                log = both(:,1) ~= both(:,2);
-                both(log,:) = 0;
-                both_vars = [var1(:,1) both(:,1)];
-                cev_both_vars = cstream2cevent(both_vars);
+                same_categories = var1_categories & var2_categories;
+                cev_both_vars = category_cstreams2cevent( ...
+                    same_categories,var1(:,1),categories);
                 record_additional_variable(subs(s), cevent_naming_matrix{1}, cev_both_vars);
             end
 
             %% var1 diff and var2 diff variables
             if ~(cevent_naming_matrix{2} == "") || ~(cevent_naming_matrix{3} == "")
-                vars_diff = (var2(:,2) ~= 0 & var1(:,2) ~= 0) & (var2(:,2) ~= var1(:,2));
-                var1_diff = var1;
-                var2_diff = var2;
-                var1_diff(~vars_diff,2) = 0;
-                var2_diff(~vars_diff,2) = 0;
-                cev_var1_bidiff = cstream2cevent(var1_diff);
-                cev_var2_bidiff = cstream2cevent(var2_diff);
+                % A category is different when the other variable is active
+                % but does not contain that category.
+                var1_diff = var1_categories & var2_active & ~var2_categories;
+                var2_diff = var2_categories & var1_active & ~var1_categories;
+                cev_var1_bidiff = category_cstreams2cevent( ...
+                    var1_diff,var1(:,1),categories);
+                cev_var2_bidiff = category_cstreams2cevent( ...
+                    var2_diff,var2(:,1),categories);
                 if ~(cevent_naming_matrix{2} == "")
                     record_additional_variable(subs(s), cevent_naming_matrix{2}, cev_var1_bidiff);
                 end
@@ -89,14 +99,12 @@ function make_combined_cevents(subexpID, cevent1, cevent2, cevent_naming_matrix)
 
             %% var1 only and var2 only variables
             if ~(cevent_naming_matrix{4} == "") || ~(cevent_naming_matrix{5} == "")
-                one_only = var2(:,2) == 0;
-                two_only = var1(:,2) == 0;
-                var1_only = var1;
-                var2_only = var2;
-                var1_only(~one_only,2) = 0;
-                var2_only(~two_only,2) = 0;
-                cev_var1_only = cstream2cevent(var1_only);
-                cev_var2_only = cstream2cevent(var2_only);
+                var1_only = var1_categories & ~var2_active;
+                var2_only = var2_categories & ~var1_active;
+                cev_var1_only = category_cstreams2cevent( ...
+                    var1_only,var1(:,1),categories);
+                cev_var2_only = category_cstreams2cevent( ...
+                    var2_only,var2(:,1),categories);
                 if ~(cevent_naming_matrix{4} == "")
                     record_additional_variable(subs(s), cevent_naming_matrix{4}, cev_var1_only);
                 end
@@ -108,3 +116,33 @@ function make_combined_cevents(subexpID, cevent1, cevent2, cevent_naming_matrix)
     end
 end
 
+
+function category_cstreams = make_category_cstreams(events,times,categories)
+% Make one binary cstream column for every category.
+
+    category_cstreams = false(numel(times),numel(categories));
+
+    for i = 1:size(events,1)
+        category_column = find(categories == events(i,3),1);
+        active = times >= events(i,1) & times < events(i,2);
+        category_cstreams(active,category_column) = true;
+    end
+end
+
+
+function cevents = category_cstreams2cevent(category_cstreams,times,categories)
+% Convert the separate category columns back into one cevent variable.
+
+    cevents = zeros(0,3);
+
+    for i = 1:numel(categories)
+        category_stream = [times zeros(numel(times),1)];
+        category_stream(category_cstreams(:,i),2) = categories(i);
+        category_cevents = cstream2cevent(category_stream);
+        cevents = [cevents; category_cevents]; %#ok<AGROW>
+    end
+
+    if ~isempty(cevents)
+        cevents = sortrows(cevents,[1 2 3]);
+    end
+end
